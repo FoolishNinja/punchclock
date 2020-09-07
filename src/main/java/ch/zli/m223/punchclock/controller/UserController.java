c62c450771e9a160297311f6dd7ae49c31ac0412
FoolishNinja <carlo.schmid1@swisscom.com>
2020-09-07 16:39:15 +0200

    Added description on how to create a new user, retreive the jwt and use it in a request

--- src/main/java/ch/zli/m223/punchclock/controller/UserController.java
+++ src/main/java/ch/zli/m223/punchclock/controller/UserController.java
@@ -21,6 +21,30 @@ public class UserController {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    /**
+     * Signup endpoint to create a new user
+     *
+     * Here is how to create new credentials and how to use them:
+     *
+     * # registers a new user
+     * curl -H "Content-Type: application/json" -X POST -d '{
+     *     "username": "admin",
+     *     "password": "password"
+     * }' http://localhost:8080/users/sign-up
+     *
+     * # logs into the application (JWT is generated)
+     * curl -i -H "Content-Type: application/json" -X POST -d '{
+     *     "username": "admin",
+     *     "password": "password"
+     * }' http://localhost:8080/login
+     *
+     * # Fetches all entries
+     * curl \
+     * -H "Authorization: Bearer xxx.yyy.zzz" \
+     * -X GET  http://localhost:8080/entries
+     *
+     * @param {ApplicationUser} user
+     */
     @PostMapping("/sign-up")
     public void signUp(@RequestBody ApplicationUser user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
